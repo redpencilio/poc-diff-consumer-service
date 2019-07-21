fe5fe60b2e2f295ddfeb286b783d081ac1722556
Erika Pauwels <erika.pauwels@gmail.com>
2019-07-21 20:02:14 +0200

    Consume files from endpoint instead of shared data volume

--- app.js
+++ app.js
@@ -1,127 +1,36 @@
-import { CronJob } from 'cron';
-import request from 'request';
-import fs from 'fs-extra';
-import { app, errorHandler, sparqlEscapeString, sparqlEscapeUri } from 'mu';
-import { querySudo as query, updateSudo as update } from '@lblod/mu-auth-sudo';
+import requestPromise from 'request-promise';
+import { app, errorHandler } from 'mu';
 
-/** Schedule export cron job */
-const cronFrequency = process.env.PACKAGE_CRON_PATTERN || '*/10 * * * * *';
+import SyncTask from './lib/sync-task';
+import { getUnconsumedFiles } from './lib/delta-file';
 
-const shareFolder = '/share';
+const INGEST_INTERVAL = process.env.INGEST_INTERVAL_MS || 5000000;
 
-new CronJob(cronFrequency, function() {
+function triggerIngest() {
   console.log(`Consuming diff files at ${new Date().toISOString()}`);
-  request.post('http://localhost/ingest/');
-}, null, true);
+  requestPromise.post('http://localhost/ingest/');
+  setTimeout( triggerIngest, INGEST_INTERVAL );
+}
 
+triggerIngest();
 
 app.post('/ingest', async function( req, res, next ) {
-  try {
-    const files = getUnconsumedFiles();
+  // TODO create sync task (should be done by a task-scheduler-service) that starts where the previous task ended
+  // TODO get not-started sync tasks sorted by creation date
+  const task = new SyncTask();
+  console.log(`Ingesting new delta files since ${task.since}`);
 
+  try {
+    const files = await getUnconsumedFiles(task.since);
+    task.files = files;
+    task.execute();
     return res.status(202).end();
   } catch(e) {
     console.log(`Something went wrong while ingesting`);
     console.trace(e);
+    // TODO write failure to store
     return next(new Error(e));
   }
 });
-
-// Helpers
-
-async function getUnconsumedFiles() {
-  const allFiles = await fs.readdir(shareFolder);
-  const result = await query(`
-    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
-    SELECT ?file WHERE {
-      GRAPH <http://mu.semte.ch/graphs/public> {
-        <http://mu.semte.ch/services/poc-diff-consumer-service> ext:hasConsumed ?file .
-      }
-    }
-  `);
-  const consumedFiles = result.results.bindings.map(b => b['file'].value);
-  const unconsumedFiles = diff(allFiles, consumedFiles);
-
-  console.log(`Found ${unconsumedFiles.length} new files to be consumed`);
-
-  for (let file of unconsumedFiles) {
-    await consume(file);
-  }
-};
-
-function diff(a, b) {
-  return a.filter(function (item) {
-    return b.indexOf(item) == -1;
-  });
-}
-
-async function consume(file) {
-  console.log(`Start consuming ${file}`);
-
-  const content = await fs.readFile(`${shareFolder}/${file}`, { encoding: 'utf-8' });
-  const changeSets = JSON.parse(content);
-
-  for (let { inserts, deletes } of changeSets) {
-    await insertTriples(inserts);
-    await deleteTriples(deletes);
-  }
-
-  await update(`
-    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
-    INSERT DATA {
-      GRAPH <http://mu.semte.ch/graphs/public> {
-        <http://mu.semte.ch/services/poc-diff-consumer-service> ext:hasConsumed ${sparqlEscapeString(file)} .
-      }
-    }
-  `);
-
-  console.log(`Successfully finished consuming ${file}`);
-};
-
-async function insertTriples(triples) {
-  // TODO insert in batches of 1000
-  if (triples.length) {
-    const statements = toStatements(triples);
-
-    await update(`
-    INSERT DATA {
-      GRAPH <http://mu.semte.ch/graphs/public> {
-        ${statements}
-      }
-    }
-  `);
-  }
-}
-
-async function deleteTriples(triples) {
-  // TODO delete in batches of 1000
-  if (triples.length) {
-    const statements = toStatements(triples);
-
-    await update(`
-    DELETE DATA {
-      GRAPH <http://mu.semte.ch/graphs/public> {
-        ${statements}
-      }
-    }
-  `);
-  }
-}
-
-function toStatements(triples) {
-  const escape = function(type, value) {
-    if (type == "uri")
-      return sparqlEscapeUri(value);
-    else
-      return sparqlEscapeString(value);
-    // TODO take datatype into account
-  };
-  return triples.map(function(t) {
-    const subject = escape(t.subject.type, t.subject.value);
-    const predicate = escape(t.predicate.type, t.predicate.value);
-    const object = escape(t.object.type, t.object.value);
-    return `${subject} ${predicate} ${object} .`;
-  }).join('');
-}
 
 app.use(errorHandler);
