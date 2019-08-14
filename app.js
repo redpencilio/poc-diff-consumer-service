d3d08537fd968edf4345dda3c3831f94565069ae
Erika Pauwels <erika.pauwels@gmail.com>
2019-08-14 23:17:23 +0200

    Schedule sync tasks in database

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import requestPromise from 'request-promise';
 import { app, errorHandler } from 'mu';
 
-import SyncTask from './lib/sync-task';
+import { getNextSyncTask, getLatestSyncTask, insertNextSyncTask } from './lib/sync-task';
 import { getUnconsumedFiles } from './lib/delta-file';
 
-const INGEST_INTERVAL = process.env.INGEST_INTERVAL_MS || 5000000;
+const INGEST_INTERVAL = process.env.INGEST_INTERVAL_MS || 5000;
 
 function triggerIngest() {
   console.log(`Consuming diff files at ${new Date().toISOString()}`);
@@ -15,21 +15,30 @@ function triggerIngest() {
 triggerIngest();
 
 app.post('/ingest', async function( req, res, next ) {
-  // TODO create sync task (should be done by a task-scheduler-service) that starts where the previous task ended
-  // TODO get not-started sync tasks sorted by creation date
-  const task = new SyncTask();
-  console.log(`Ingesting new delta files since ${task.since}`);
+  const task = await getNextSyncTask();
+  if (task) {
+    console.log(`Ingesting new delta files since ${task.since.toISOString()}`);
+    try {
+      const files = await getUnconsumedFiles(task.since);
+      task.files = files;
+      task.execute();
+      return res.status(202).end();
+    } catch(e) {
+      console.log(`Something went wrong while ingesting`);
+      console.trace(e);
+      // TODO write failure to store
+      return next(new Error(e));
+    }
+  } else {
+    console.log(`No sync task found`);
+    const latestTask = getLatestSyncTask();
 
-  try {
-    const files = await getUnconsumedFiles(task.since);
-    task.files = files;
-    task.execute();
-    return res.status(202).end();
-  } catch(e) {
-    console.log(`Something went wrong while ingesting`);
-    console.trace(e);
-    // TODO write failure to store
-    return next(new Error(e));
+    if (latestTask) {
+      await insertNextSyncTask(latestTask.until);
+    } else {
+      await insertNextSyncTask();
+    }
+    return res.status(200).end();
   }
 });
 
